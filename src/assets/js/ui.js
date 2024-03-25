fe9d7feb014f8722e18c4e2336d3231602eacac4
park <rmstn133@gmail.com>
2024-03-25 18:44:18 +0900

    push

---
+++ src/assets/js/ui.js
@@ -0,0 +1,20 @@
+var Header = {
+  init: function () {
+    // this.aside();
+  },
+};
+
+var Common = {
+  init: function () {
+    this.common();
+    this.datepickerRun();
+  },
+
+  common: function () {
+    // confirm 모달
+  },
+  datepickerRun: function () {},
+};
+
+Header.init();
+Common.init();
